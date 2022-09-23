efc4fce069892a977436133987f4d631b3565323
Ilea Cristian <ileacristian@gmail.com>
2022-09-23 14:28:24 +0300

    Initial Commit

---
+++ LoxCompiler/LoxCompilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoxCompilerApp.swift
+//  LoxCompiler
+//
+//  Created by Cristian Ilea on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoxCompilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
